48f54ea4c37a39d601c8eebf9a2276c129a538cc
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-12-11 10:44:23 -0800

    initial commit

---
+++ src/webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+function getStyleUse(bundleFilename) {
+    return [{
+            loader: 'file-loader',
+            options: {
+                name: bundleFilename,
+            },
+        }, {
+            loader: 'extract-loader'
+        },
+        {
+            loader: 'css-loader',
+        },
+        {
+            loader: 'postcss-loader',
+            options: {
+                plugins: () => [autoprefixer()]
+            }
+        },
+        {
+            loader: 'sass-loader',
+            options: {
+                includePaths: ['./node_modules'],
+            }
+        }
+    ];
+}
+
+module.exports = [{
+        entry: {
+            'bundle.min.css': [
+                path.resolve(__dirname, 'styles/main.scss'),
+            ]
+        },
+        output: {
+            // This is necessary for webpack to compile, but we never reference this js file.
+            filename: '[name]',
+            path: path.resolve(__dirname, '../build/styles/'),
+        },
+        module: {
+            rules: [{
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({
+                        use: [{
+                                loader: 'css-loader'
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    includePaths: [path.resolve(__dirname, 'node_modules/')]
+                                }
+                            }
+                        ],
+                    })
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        use: 'css-loader'
+                    })
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }],
+                },
+            ],
+        },
+        plugins: [
+            new ExtractTextPlugin("bundle.min.css"),
+        ],
+        watch: true,
+    },
+    {
+        entry: {
+            'bundle.min.js': [
+                path.resolve(__dirname, 'scripts/main.js'),
+            ]
+        },
+        output: {
+            filename: '[name]',
+            path: path.resolve(__dirname, '../build/scripts/'),
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    },
+                }],
+            }]
+        },
+        watch: true,
+    }
+];
