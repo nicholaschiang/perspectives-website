f3f1da98d992fcee52e93e1265b834b6d6240435
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-12-11 21:32:57 -0800

    fix: update timeline animation values

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,72 +1,76 @@
 $(() => {
-  let stickyTop = 0,
-  scrollTarget = false;
+    let stickyTop = 0,
+        scrollTarget = false;
 
-  let timeline = $('.timeline__nav'),
-  items = $('li', timeline),
-  milestones = $('.timeline__section .milestone'),
-  offsetTop = parseInt(timeline.css('top'));
+    let timeline = $('.timeline__nav'),
+        items = $('li', timeline),
+        milestones = $('.timeline__section .milestone'),
+        offsetTop = parseInt(timeline.css('top'));
 
-  const TIMELINE_VALUES = {
-    start: 190,
-    step: 30 };
+    const TIMELINE_VALUES = {
+        start: 100,
+        step: 40
+    };
 
 
-  $(window).resize(function () {
-    timeline.removeClass('fixed');
+    $(window).resize(function() {
+        timeline.removeClass('fixed');
 
-    stickyTop = timeline.offset().top - offsetTop;
+        stickyTop = timeline.offset().top - offsetTop;
 
-    $(window).trigger('scroll');
-  }).trigger('resize');
+        $(window).trigger('scroll');
+    }).trigger('resize');
 
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > stickyTop) {
-      timeline.addClass('fixed');
-    } else {
-      timeline.removeClass('fixed');
-    }
-  }).trigger('scroll');
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > stickyTop) {
+            timeline.addClass('fixed');
+        } else {
+            timeline.removeClass('fixed');
+        }
+    }).trigger('scroll');
 
-  items.find('span').click(function () {
-    let li = $(this).parent(),
-    index = li.index(),
-    milestone = milestones.eq(index);
+    items.find('span').click(function() {
+        let li = $(this).parent(),
+            index = li.index(),
+            milestone = milestones.eq(index);
 
-    if (!li.hasClass('active') && milestone.length) {
-      scrollTarget = index;
+        if (!li.hasClass('active') && milestone.length) {
+            scrollTarget = index;
 
-      let scrollTargetTop = milestone.offset().top - 80;
+            let scrollTargetTop = milestone.offset().top - 80;
 
-      $('html, body').animate({ scrollTop: scrollTargetTop }, {
-        duration: 400,
-        complete: function complete() {
-          scrollTarget = false;
-        } });
+            $('html, body').animate({
+                scrollTop: scrollTargetTop
+            }, {
+                duration: 400,
+                complete: function complete() {
+                    scrollTarget = false;
+                }
+            });
 
-    }
-  });
+        }
+    });
 
-  $(window).scroll(function () {
-    let viewLine = $(window).scrollTop() + $(window).height() / 3,
-    active = -1;
+    $(window).scroll(function() {
+        let viewLine = $(window).scrollTop() + $(window).height() / 3,
+            active = -1;
 
-    if (scrollTarget === false) {
-      milestones.each(function () {
-        if ($(this).offset().top - viewLine > 0) {
-          return false;
+        if (scrollTarget === false) {
+            milestones.each(function() {
+                if ($(this).offset().top - viewLine > 0) {
+                    return false;
+                }
+
+                active++;
+            });
+        } else {
+            active = scrollTarget;
         }
 
-        active++;
-      });
-    } else {
-      active = scrollTarget;
-    }
+        timeline.css('top', -1 * active * TIMELINE_VALUES.step + TIMELINE_VALUES.start + 'px');
 
-    timeline.css('top', -1 * active * TIMELINE_VALUES.step + TIMELINE_VALUES.start + 'px');
+        items.filter('.active').removeClass('active');
 
-    items.filter('.active').removeClass('active');
-
-    items.eq(active != -1 ? active : 0).addClass('active');
-  }).trigger('scroll');
+        items.eq(active != -1 ? active : 0).addClass('active');
+    }).trigger('scroll');
 });
